d022eb7530d55541a4643356a860505f367f7966
JohnChristianMaliwat <maliwat.johnchristian.soleta@gmail.com>
2024-03-12 15:56:22 +0800

    My commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+function generateRandomNumber() {
+    let randomNum = Math.floor(Math.random() * 15) + 1;
+    
+    if(randomNum >= 5){
+        console.log(`Random Number is > = to 5`);
+    
+    } else {
+        console.log(`Random Number is < = to 5`);
+
+    }
+    return console.log(randomNum);
+}
