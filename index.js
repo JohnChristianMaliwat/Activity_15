4f551a16d192865a1fec8951df871a687257f356
JohnChristianMaliwat <maliwat.johnchristian.soleta@gmail.com>
2024-03-14 15:43:28 +0800

    Additional Commit

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 console.log("Hello World");
 
-function generateRandomNumber() {
-    let randomNum = Math.floor(Math.random() * 15) + 1;
+generateRandomNumber(100)
+function generateRandomNumber(max) {
+    let randomNum = Math.floor(Math.random() * max) + 1;
     
-    if(randomNum >= 5){
-        console.log(`Random Number is > = to 5`);
-    
-    } else {
-        console.log(`Random Number is < = to 5`);
+    if(randomNum <= 15){
+        return console.log(`Random Number ${randomNum} is in B. 1 - 15`);
 
+    } else if (randomNum >= 16 && randomNum <= 30) {
+      return console.log(`Random number ${randomNum} is in I. 16 - 30`);
+
+    } else if (randomNum >= 31 && randomNum <= 45) {
+        return console.log(`Random number ${randomNum} is in N. 31 - 45`);
+
+    } else if (randomNum >= 46 && randomNum <= 60) {
+        return console.log(`Random number ${randomNum} is in G. 46 - 61`);
+
+    } else if (randomNum >= 61 && randomNum <= 75) {
+        return console.log(`Random number ${randomNum} is in O. 62 - 75`);
+
+    } else {
+        return console.log(`Random Number is invalid`);
     }
-    return console.log(randomNum);
+    
 }
