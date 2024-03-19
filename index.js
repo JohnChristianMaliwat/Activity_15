ba2d6b2e0faa77ec9fa308fc5eec60e3aff1e1e9
JohnChristianMaliwat <maliwat.john.christian.soleta@gmail.com>
2024-03-19 15:28:48 +0800

    Up to date Commit

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
 console.log("Hello World");
 
-generateRandomNumber(100)
+
+let B = false,
+    I = false,
+    N = false,
+    G = false,
+    O = false;
+
+generateRandomNumber(75)
+
+
 function generateRandomNumber(max) {
     let randomNum = Math.floor(Math.random() * max) + 1;
     
     if(randomNum <= 15){
-        return console.log(`Random Number ${randomNum} is in B. 1 - 15`);
+        B = true;
+     console.log(`Random Number ${randomNum} is in B. 1 - 15`);
+  
 
     } else if (randomNum >= 16 && randomNum <= 30) {
-      return console.log(`Random number ${randomNum} is in I. 16 - 30`);
-
+        I = true;
+       console.log(`Random number ${randomNum} is in I. 16 - 30`);
+      
     } else if (randomNum >= 31 && randomNum <= 45) {
-        return console.log(`Random number ${randomNum} is in N. 31 - 45`);
+        N = true;
+        console.log(`Random number ${randomNum} is in N. 31 - 45`);
+
 
     } else if (randomNum >= 46 && randomNum <= 60) {
-        return console.log(`Random number ${randomNum} is in G. 46 - 61`);
+        G = true;
+       console.log(`Random number ${randomNum} is in G. 46 - 61`);
+
 
     } else if (randomNum >= 61 && randomNum <= 75) {
-        return console.log(`Random number ${randomNum} is in O. 62 - 75`);
+        O = true;
+     console.log(`Random number ${randomNum} is in O. 62 - 75`);
+  
 
     } else {
-        return console.log(`Random Number is invalid`);
+     console.log(`Random Number is invalid`);
     }
+
+     if (B == true && I == true && N == true && G == true && O == true) {
+        console.log(`BINGOOO!!`);
+     }
     
 }
